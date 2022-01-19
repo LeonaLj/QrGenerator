534977bb3e373f2af0828a45c7fef65986b9be44
LeonaLj <ljubicicleona@gmail.com>
2022-01-19 17:37:47 +0100

    First commit

--- src/components/QRgenerator.js
+++ src/components/QRgenerator.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import QRCode from 'qrcode'
 import './QRgenerator.css'
 import { Button, Container, Form, Row, Col } from 'react-bootstrap'
+import { AiOutlineDownload } from 'react-icons/ai'
+import { ImQrcode } from 'react-icons/im'
+
 
 function QRgenerator() {
 
@@ -31,16 +34,27 @@ function QRgenerator() {
         <Container id="app-container">
             <Row>
                 <Col>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Control type="text" name="name" onChange={urlInput} />
-                    </Form>
-                    <Button className="mt-3" variant="dark" size="lg" onClick={generateQrCode}>Generate</Button>
+                    <ImQrcode className="logo-icon" />
+                </Col>
+            </Row>
+            <Row id="row">
+                <Col md={8} sm={6} className="generate-container div-css">
+                    <div className="gnr-div">
+                        <Form onSubmit={handleSubmit}>
+                            <h1>Enter your link</h1>
+                            <Form.Control className="input" type="text" size="lg" name="name" placeholder="Your link goes here" onFocus={(e) => e.target.placeholder = ""} onBlur={(e) => e.target.placeholder = "Your link goes here"} onChange={urlInput} />
+                        </Form>
+                        <Button className="mt-5 generate-btn fw-bold" size="lg" onClick={generateQrCode}>Generate</Button>
+                    </div>
                 </Col>
 
-                <Col>
-                    {imageUrl ? (<img className="qr-img" src={imageUrl} alt="qr-img" />) : null}
+                <Col md={4} sm={6} className="qr-container div-css">
                     <div>
-                        <a href={imageUrl} download><Button className="mt-3" variant="dark" size="lg">Download QR</Button></a>
+                        <h3>This is your QR code</h3>
+                    </div>
+                    <div className="qr-div">
+                        {imageUrl ? (<img className="qr-img" src={imageUrl} alt="qr-img" />) : null}
+                        <a href={imageUrl} download><Button className="mt-3 download-btn fw-bold" size="lg"><AiOutlineDownload className="icon" /> Download QR</Button></a>
                     </div>
                 </Col>
             </Row>
